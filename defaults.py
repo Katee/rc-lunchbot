f018666245a32d6affa437e2ccf1db6c9a0c8ac8
katee <hello@kate.io>
2017-06-28 17:59:56 -0400

    Don't fail in defaults.py if a required env isn't set

--- defaults.py
+++ defaults.py
@@ -3,8 +3,8 @@
 import os
 
 defaults = {
-    'zulip_username': os.environ['ZULIP_LUNCHBOT_EMAIL'],
-    'zulip_api_key': os.environ['ZULIP_LUNCHBOT_KEY'],
+    'zulip_username': os.getenv('ZULIP_LUNCHBOT_EMAIL', None),
+    'zulip_api_key': os.getenv('ZULIP_LUNCHBOT_KEY', None),
     'zulip_site': os.getenv('ZULIP_LUNCHBOT_SITE', 'https://recurse.zulipchat.com'),
     'zulip_stream': os.getenv('ZULIP_LUNCHBOT_STREAM', 'lunchbot-staging'),
     'date_overrides': os.getenv('ZULIP_LUNCHBOT_DATE_OVERRIDES', ''),
